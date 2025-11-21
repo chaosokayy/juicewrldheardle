6194d1bda710fa983a2de34f20302466a31954be
chaosokayy <eshanyboy@gmail.com>
2025-11-21 15:35:23 -0500

    a

--- server.js
+++ server.js
@@ -100,5 +100,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Proxying requests to juicewrldapi.com to bypass CORS`);
 });
